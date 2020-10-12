ecdd84ee0c705af9a44c288faf2daaec0a8f485c
Amazigh Djebarri <djebarri.amazigh@gmail.com>
2020-10-12 15:41:45 +0200

    UPDATE - add jacoco for code coverage (with minimum cover rate)

---
+++ src/test/java/org/nearmi/core/DumyTest.java
@@ -0,0 +1,11 @@
+package org.nearmi.core;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class DumyTest {
+    @Test
+    public void test() {
+        Assertions.assertTrue(true);
+    }
+}
